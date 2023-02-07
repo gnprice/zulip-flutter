b441ff169663ba76e1ffea90aa7f4cf2a2543983
Greg Price <gnprice@gmail.com>
2023-02-06 22:04:06 -0800

    wip try power-law scroll physics

--- lib/widgets/message_list.dart
+++ lib/widgets/message_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -76,6 +78,7 @@ class _MessageListState extends State<MessageList> {
     final length = model!.messages.length;
     assert(model!.contents.length == length);
     return StickyHeaderListView.builder(
+        physics: const PowerLawScrollPhysics(),
         itemCount: length,
         // Setting reverse: true means the scroll starts at the bottom.
         // Flipping the indexes (in itemBuilder) means the start/bottom
@@ -318,3 +321,119 @@ final _kMessageTimestampStyle = TextStyle(
     fontSize: 12,
     fontWeight: FontWeight.w400,
     color: const HSLColor.fromAHSL(0.4, 0, 0, 0.2).toColor());
+
+class PowerLawScrollPhysics extends ClampingScrollPhysics {
+  const PowerLawScrollPhysics({super.parent});
+
+  @override
+  PowerLawScrollPhysics applyTo(ScrollPhysics? ancestor) {
+    return PowerLawScrollPhysics(parent: buildParent(ancestor));
+  }
+
+  @override
+  Simulation? createBallisticSimulation(ScrollMetrics position, double velocity) {
+    final base = super.createBallisticSimulation(position, velocity);
+    if (base is! ClampingScrollSimulation)
+      return base;
+    return PowerLawScrollSimulation(
+        position: position.pixels, velocity: velocity, tolerance: tolerance);
+  }
+
+  @override
+  Simulation? updateBallisticSimulation(Simulation oldSimulation, ScrollMetrics position, double time) {
+    return createBallisticSimulation(position, oldSimulation.dx(time));
+  }
+}
+
+/// An implementation of scroll physics that aligns with Android.
+///
+/// This travels the same total distance on a given fling as the native Android
+/// scroll physics does.  The curve has been adjusted in order to make it
+/// ballistic, so that the deceleration at any moment is a function only of the
+/// current velocity regardless of how long the simulation has been running.
+/// This makes it compatible with Flutter's scroll-physics protocol, where a
+/// simulation can be restarted many times using only its current velocity.
+class PowerLawScrollSimulation extends Simulation {
+  PowerLawScrollSimulation(
+      {super.tolerance, required this.position, required this.velocity,
+        this.offsetTime = 0.0}) {
+    _duration = _flingDuration(velocity);
+    debugPrint('simulation: v0=${velocity.toStringAsFixed(1)}, times ${offsetTime.toStringAsFixed(4)}/${_duration.toStringAsFixed(4)}');
+  }
+
+  final double position;
+  final double velocity;
+  final double offsetTime;
+
+  late double _duration;
+
+  static const friction = 0.015;
+
+  // See DECELERATION_RATE.
+  static final double _kDecelerationRate = math.log(0.78) / math.log(0.9);
+
+  // See computeDeceleration().
+  static double _decelerationForFriction(double friction) {
+    return friction * 61774.04968;
+  }
+
+  // See getSplineFlingDuration(). Returns a value in seconds.
+  double _flingDuration(double velocity) {
+    // See mPhysicalCoeff.
+    final double scaledFriction = friction * _decelerationForFriction(0.84);
+
+    // This is scaled so that the total distance traveled is the same as on Android.
+    return _kDecelerationRate * kInflexion *
+        math.pow(velocity.abs() * kInflexion / scaledFriction,
+            1 / (_kDecelerationRate - 1.0));
+  }
+
+  // See INFLEXION.
+  static const kInflexion = 0.35;
+
+  double _flingDistancePenetration(double t) {
+    return 1.0 - math.pow(1.0 - t, _kDecelerationRate);
+  }
+
+  double _flingVelocityPenetration(double t) {
+    return math.pow(1.0 - t, _kDecelerationRate - 1.0) as double;
+  }
+
+  @override
+  double x(double time) {
+    final double t = clampDouble((time + offsetTime) / _duration, 0.0, 1.0);
+    // debugPrint('x: time ${time.toStringAsFixed(4)}/${_duration.toStringAsFixed(4)} -> ${t.toStringAsFixed(4)}'
+    //   + ' -> ${_flingDistancePenetration(t).toStringAsFixed(4)}'
+    //   + ' -> ${(position + velocity*_duration/_kDecelerationRate * _flingDistancePenetration(t)).toStringAsFixed(1)}');
+    return position + velocity*_duration/_kDecelerationRate * _flingDistancePenetration(t);
+  }
+
+  @override
+  double dx(double time) {
+    final double t = clampDouble((time + offsetTime) / _duration, 0.0, 1.0);
+    // debugPrint('dx: time ${time.toStringAsFixed(4)}/${_duration.toStringAsFixed(4)} -> ${t.toStringAsFixed(4)}'
+    //     + ' -> ${_flingVelocityPenetration(t).toStringAsFixed(4)}'
+    //     + ' -> ${(velocity * _flingVelocityPenetration(t)).toStringAsFixed(1)}');
+    return velocity * _flingVelocityPenetration(t);
+  }
+
+  @override
+  bool isDone(double time) {
+    return (time + offsetTime) >= _duration;
+  }
+}
+
+// borrowed from framework's foundation/math.dart
+double clampDouble(double x, double min, double max) {
+  assert(min <= max && !max.isNaN && !min.isNaN);
+  if (x < min) {
+    return min;
+  }
+  if (x > max) {
+    return max;
+  }
+  if (x.isNaN) {
+    return max;
+  }
+  return x;
+}
