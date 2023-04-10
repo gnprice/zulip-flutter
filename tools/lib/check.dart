4b9dbd1e0cfa1aa0d74626945c117b7d8598151b
Greg Price <greg@zulip.com>
2023-04-10 13:03:35 -0700

    wip factor a bit better

--- tools/lib/check.dart
+++ tools/lib/check.dart
@@ -2,10 +2,28 @@
 import 'dart:io';
 
 Future<void> main() async {
+  bool hadFailure = false;
+  final result = await checkAnalyze();
+  if (result.failure != null) {
+    print(result.failure!.msg);
+    hadFailure = true;
+  }
+  exit(hadFailure ? 1 : 0);
+}
+
+typedef CheckResult = ({CheckFailure? failure});
+
+typedef CheckFailure = ({String msg});
+
+Future<CheckResult> checkAnalyze() async {
   final result = await Process.run('flutter', ['analyze']);
   if (result.exitCode != 0) {
-    print("error: flutter analyze failed");
-    print(result.stdout);
-    print(result.stderr);
+    return (failure: (msg:
+      // ignore: prefer_interpolation_to_compose_strings
+      'error: flutter analyze failed\n'
+      + result.stdout
+      + result.stderr
+    ));
   }
+  return (failure: null);
 }
