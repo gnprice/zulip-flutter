273176051abf33bebf9a41a90cb8c9d41decd56a
Greg Price <greg@zulip.com>
2023-06-16 21:01:16 -0700

    WIP add test Firebase options

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+
+import 'package:firebase_core/firebase_core.dart';
+
+// TODO this is for Android
+// TODO copy context comment from zulip-mobile:android/app/src/main/res/values/firebase.xml
+const kFirebaseOptions = FirebaseOptions(
+  appId: '1:835904834568:android:6ae61ae43a7c3410',
+  messagingSenderId: '835904834568',
+  projectId: 'zulip-android',
+
+  // A Google Cloud "API key" is a very different kind of thing
+  // from a Zulip "API key".  Docs: https://cloud.google.com/docs/authentication/api-keys
+  apiKey: 'AIzaSyC6kw5sqCYjxQl2Lbd_8MDmc1lu2EG0pY4',
+);
