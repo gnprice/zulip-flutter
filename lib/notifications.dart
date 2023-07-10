a178b868e5a86faaf47bffd1c6b4beba50a52be9
Greg Price <greg@zulip.com>
2023-07-10 16:36:50 -0700

    verify we have bug on opening one of many notifs

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -107,7 +107,8 @@ class NotificationService {
           // TODO inbox-style
 
           // TODO plugin sets PendingIntent.FLAG_UPDATE_CURRENT; is that OK?
-          // TODO plugin's setContentIntent doesn't avoid dupe URLs, or set our Intent flags; is that OK?
+          // TODO plugin's setContentIntent doesn't set our Intent flags; is that OK?
+          // TODO all notifs lead to convo of latest notif, because plugin's setContentIntent doesn't avoid dupe URLs
         )));
     }
   }
