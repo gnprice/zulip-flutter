9fbdd3600d7e31bb264d12d81bdeb33036b00eac
Greg Price <greg@zulip.com>
2023-07-10 14:46:49 -0700

    wip notifs work! crudely: TODO inbox-style, sounds, background, action, oh and ios

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'api/notifications.dart';
 import 'log.dart';
 import 'model/binding.dart';
+import 'widgets/app.dart';
 
 class NotificationService {
   static NotificationService get instance => (_instance ??= NotificationService._());
@@ -71,7 +73,64 @@ class NotificationService {
     print(message.data);
     final data = FcmMessage.fromJson(message.data);
     if (data is MessageFcmMessage) {
+      _ensureChannel();
       print('content: ${data.content}');
+      FlutterLocalNotificationsPlugin().show(
+        _kNotificationId,
+        switch (data) {
+          // TODO stream messages
+          MessageFcmMessage(:var pmUsers?, :var senderFullName) =>
+            '$senderFullName to you and ${pmUsers.length - 2} others', // TODO(i18n), also plural; TODO use others' names, from data
+          MessageFcmMessage(:var senderFullName) =>
+            senderFullName,
+        },
+        data.content, // TODO
+        NotificationDetails(android: AndroidNotificationDetails(
+          _kChannelId, 'channel name',
+          tag: _conversationKey(data),
+          color: kZulipBrandColor,
+          icon: 'zulip_notification', // TODO vary for debug
+          // TODO inbox-style
+        )));
     }
   }
+
+  String _conversationKey(MessageFcmMessage data) {
+    final groupKey = _groupKey(data);
+    final conversation = switch (data) {
+      // TODO stream messages
+      MessageFcmMessage(:var pmUsers?) => 'group-dm:$pmUsers', // TODO
+      MessageFcmMessage(:var senderId) => 'dm:$senderId',
+    };
+    return '$groupKey|$conversation';
+  }
+
+  String _groupKey(FcmMessageWithIdentity data) {
+    // The realm URL can't contain a `|`, because `|` is not a URL code point:
+    //   https://url.spec.whatwg.org/#url-code-points
+    return "${data.realmUri}|${data.userId}";
+  }
+
+  void _ensureChannel() async { // TODO "ensure"
+    final plugin = FlutterLocalNotificationsPlugin();
+    await plugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(AndroidNotificationChannel(
+        _kChannelId,
+        'Messages', // TODO(i18n)
+        importance: Importance.high,
+        enableLights: true,
+        vibrationPattern: _kVibrationPattern,
+        // TODO sound
+      ));
+  }
 }
+
+const _kNotificationId = 435;
+
+const _kChannelId = 'messages-1';
+
+/// The vibration pattern we set for notifications.
+// We try to set a vibration pattern that, with the phone in one's pocket,
+// is both distinctly present and distinctly different from the default.
+// Discussion: https://chat.zulip.org/#narrow/stream/48-mobile/topic/notification.20vibration.20pattern/near/1284530
+final _kVibrationPattern = Int64List.fromList([0, 125, 100, 450]);
