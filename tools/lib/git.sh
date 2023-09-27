5d0fc0705aa7525be75515ed0907c8a9acdfa380
Greg Price <greg@zulip.com>
2023-09-26 21:21:15 -0700

    wip check/drift: handle untracked too; TODO test

--- tools/lib/git.sh
+++ tools/lib/git.sh
@@ -25,3 +25,18 @@ check_no_uncommitted_changes()
         return 1
     fi
 }
+
+no_untracked_files()
+{
+    if git ls-files --others --exclude-standard -- "$@" \
+            | grep -q .; then
+        return 1
+    fi
+}
+
+git_status_short()
+{
+    # --untracked-files=normal is the default; but the user's local config
+    # may have overridden it, so we specify explicitly.
+    git status --short --untracked-files=normal -- "$@"
+}
