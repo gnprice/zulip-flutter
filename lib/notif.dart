2675274e88aa8d051dd91c8e0863bc42cce354ac
Greg Price <greg@zulip.com>
2023-10-13 18:10:02 -0700

    wip add comments

--- lib/notif.dart
+++ lib/notif.dart
@@ -20,11 +20,20 @@ class NotificationService {
   Future<void> _getToken() async {
     final result = await ZulipBinding.instance.firebaseMessaging.getToken(); // TODO(log) if null
     assert(debugLog("notif token: $result"));
+    // On a typical launch of the app (other than the first one after install),
+    // this is the only way we learn the token value; onTokenRefresh never fires.
     token.value = result;
   }
 
   void _onTokenRefresh(String value) {
-    assert(debugLog("refreshed token: $value"));
+    assert(debugLog("new notif token: $value"));
+    // On first launch after install, our [FirebaseMessaging.getToken] call
+    // causes this to fire, followed by completing its own future so that
+    // `_getToken` sees the value as well.  So in that case this is redundant.
+    //
+    // Subsequently, though, this can also potentially fire on its own, if for
+    // some reason the FCM system decides to replace the token.  So both paths
+    // need to save the value.
     token.value = value;
   }
 }
