d78dbe819bd373aa72555d5157d36757577f188f
Greg Price <greg@zulip.com>
2023-10-16 16:20:44 -0700

    wip fixup firebase options add comments

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,13 +1,26 @@
-
 import 'package:firebase_core/firebase_core.dart';
 
-// TODO copy context comment from zulip-mobile:android/app/src/main/res/values/firebase.xml
+/// Configuration used for receiving notifications on Android.
+///
+/// This set of options is used for receiving notifications
+/// through the Zulip notification bouncer service:
+///   https://zulip.readthedocs.io/en/latest/production/mobile-push-notifications.html
+///
+/// These values represent public identifiers for that service
+/// as an application registered with the relevant Google service:
+/// we deliver Android notifications through Firebase Cloud Messaging (FCM).
+/// The values are derived from a `google-services.json` file.
+/// For details, see:
+///   https://developers.google.com/android/guides/google-services-plugin#processing_the_json_file
 const kFirebaseOptionsAndroid = FirebaseOptions(
   appId: '1:835904834568:android:6ae61ae43a7c3410',
   messagingSenderId: '835904834568',
   projectId: 'zulip-android',
 
-  // A Google Cloud "API key" is a very different kind of thing
-  // from a Zulip "API key".  Docs: https://cloud.google.com/docs/authentication/api-keys
+  // Despite the name, this Google Cloud "API key" is a very different kind
+  // of thing from a Zulip "API key".  In particular, it's designed to be
+  // included in published builds of client applications, and therefore
+  // fundamentally public.  See docs:
+  //   https://cloud.google.com/docs/authentication/api-keys
   apiKey: 'AIzaSyC6kw5sqCYjxQl2Lbd_8MDmc1lu2EG0pY4',
 );
