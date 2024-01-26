b85a13a5f79fa2f6a19a18d36e01a5ce55dc757e
Greg Price <greg@zulip.com>
2024-01-25 22:25:15 -0800

    wip FirstSliverTopScrollView, to control paint order

--- lib/widgets/scrolling.dart
+++ lib/widgets/scrolling.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 /// A [SingleChildScrollView] that always shows a Material [Scrollbar].
 ///
@@ -35,5 +36,131 @@ class _SingleChildScrollViewWithScrollbarState
         controller: controller,
         scrollDirection: widget.scrollDirection,
         child: widget.child));
+  }
+}
+
+/// A [CustomScrollView] where the first sliver in [slivers] paints on top.
+///
+/// This is just like [CustomScrollView] except that the paint order runs
+/// through the [slivers] list from end to start, and the hit-test order runs
+/// from start to end.  This means that the sliver at the start of [slivers]
+/// is effectively on top in the z-direction, with the next sliver below it,
+/// and so on.
+///
+/// When [center] is null or corresponds to the first sliver in the list, this
+/// is the same behavior as [CustomScrollView].  Otherwise, [CustomScrollView]
+/// has the [center] sliver on top in the z-direction, followed by the slivers
+/// after it to the end of [slivers], followed by the slivers before [center]
+/// in reverse order, with the first sliver in the list at the bottom in the
+/// z-direction.
+// TODO(upstream): add an option [ScrollView.zOrder]?  (An enum, or possibly
+//   a delegate.)  Or at minimum document on [ScrollView.center] the
+//   existing behavior, which is counterintuitive.
+//   Nearest related upstream feature requests I find are for a "z-index",
+//   for CustomScrollView, Column, and Stack respectively:
+//     https://github.com/flutter/flutter/issues/121173#issuecomment-1712825747
+//     https://github.com/flutter/flutter/issues/121173
+//     https://github.com/flutter/flutter/issues/70836
+//   A delegate would give enough flexibility for that and much else,
+//   but I'm not sure how many use cases wouldn't be covered by a small enum.
+//
+// TODO: perh sticky_header should configure a FirstSliverTopScrollView automatically?
+class FirstSliverTopScrollView extends CustomScrollView {
+  const FirstSliverTopScrollView({
+    super.key,
+    super.scrollDirection,
+    super.reverse,
+    super.controller,
+    super.primary,
+    super.physics,
+    super.scrollBehavior,
+    // super.shrinkWrap, // omitted, always false
+    super.center,
+    super.anchor,
+    super.cacheExtent,
+    super.slivers,
+    super.semanticChildCount,
+    super.dragStartBehavior,
+    super.keyboardDismissBehavior,
+    super.restorationId,
+    super.clipBehavior,
+  });
+
+  @override
+  Widget buildViewport(BuildContext context, ViewportOffset offset,
+      AxisDirection axisDirection, List<Widget> slivers) {
+    return FirstSliverTopViewport(
+      axisDirection: axisDirection,
+      offset: offset,
+      slivers: slivers,
+      cacheExtent: cacheExtent,
+      center: center,
+      anchor: anchor,
+      clipBehavior: clipBehavior,
+    );
+  }
+}
+
+class FirstSliverTopViewport extends Viewport {
+  FirstSliverTopViewport({
+    super.key,
+    super.axisDirection,
+    super.crossAxisDirection,
+    super.anchor,
+    required super.offset,
+    super.center,
+    super.cacheExtent,
+    super.cacheExtentStyle,
+    super.slivers,
+    super.clipBehavior,
+  });
+
+  @override
+  RenderViewport createRenderObject(BuildContext context) {
+    return RenderFirstSliverTopViewport(
+      axisDirection: axisDirection,
+      crossAxisDirection: crossAxisDirection ?? Viewport.getDefaultCrossAxisDirection(context, axisDirection),
+      anchor: anchor,
+      offset: offset,
+      cacheExtent: cacheExtent,
+      cacheExtentStyle: cacheExtentStyle,
+      clipBehavior: clipBehavior,
+    );
+  }
+}
+
+class RenderFirstSliverTopViewport extends RenderViewport {
+  RenderFirstSliverTopViewport({
+    super.axisDirection,
+    required super.crossAxisDirection,
+    required super.offset,
+    super.anchor,
+    super.children,
+    super.center,
+    super.cacheExtent,
+    super.cacheExtentStyle,
+    super.clipBehavior,
+  });
+
+  @override
+  Iterable<RenderSliver> get childrenInPaintOrder {
+    final List<RenderSliver> children = <RenderSliver>[];
+    RenderSliver? child = lastChild;
+    while (child != null) {
+      children.add(child);
+      child = childBefore(child);
+    }
+    return children;
+  }
+
+  @override
+  Iterable<RenderSliver> get childrenInHitTestOrder {
+    final List<RenderSliver> children = <RenderSliver>[];
+    RenderSliver? child = firstChild;
+    while (child != null) {
+      children.add(child);
+      child = childAfter(child);
+    }
+    return children;
   }
 }
