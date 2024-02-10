eb2f6d85b04f52ca00e42c9a3695cf81578b2b75
Greg Price <greg@zulip.com>
2024-02-09 18:13:30 -0800

    wip cut most prints, crank numTrials back up

--- test/backoff_test.dart
+++ test/backoff_test.dart
@@ -8,9 +8,7 @@ import 'package:zulip/backoff.dart';
 
 Future<Duration> measureWait(Future<void> future) async {
   final start = clock.now();
-  print('    measure: started $start');
   await future;
-  print('    measure: done');
   return clock.now().difference(start);
 }
 
@@ -37,7 +35,6 @@ class ErrorResult<T> extends Result<T> {
 T fakeAsyncBetter<T>(Future<T> Function(FakeAsync) callback) {
   // cf https://stackoverflow.com/a/62676919
   return fakeAsync((binding) {
-    print('${clock.now()} outer');
     Result<T>? result;
     (() async {
       try {
@@ -47,13 +44,9 @@ T fakeAsyncBetter<T>(Future<T> Function(FakeAsync) callback) {
         result = ErrorResult(e);
       }
     })();
-    print('${clock.now()} outer: called');
     while (result == null) {
       binding.flushTimers();
-      print('${clock.now()} outer: flushed');
     }
-    print('${clock.now()} outer: done');
-    // binding.flushMicrotasks();
     switch (result!) {
       case SuccessResult(:var value): return value;
       case ErrorResult(:var error): throw error;
@@ -84,38 +77,13 @@ void main() {
     print(result);
 
     return;
-    fakeAsync((binding) async {
-      print('${clock.now()} hi');
-
-      final delay = Future.delayed(Duration(milliseconds: 100));
-      print('${clock.now()} future made');
-      print(binding.pendingTimersDebugString);
-
-      bool active = true;
-      final after = delay.whenComplete(() => active = false);
-      print('${clock.now()} set whenComplete');
-      print(binding.pendingTimersDebugString);
-
-      binding.flushTimers();
-      print('${clock.now()} flushed');
-      print(binding.pendingTimersDebugString);
-
-      while (active) {
-        binding.flushMicrotasks();
-        print('${clock.now()} flushed microtasks');
-        print(binding.pendingTimersDebugString);
-      }
-
-      await after;
-      print('${clock.now()} awaited');
-    });
   });
 
   test('BackoffMachine timeouts are random from zero to 100ms, 200ms, 400ms, ...', () {
     // This is a randomized test.  [numTrials] is chosen so that the failure
     // probability < 1e-9.  There are 2 * 11 assertions, and each one has a
     // failure probability < 1e-12; see below.
-    const numTrials = 3; //100;
+    const numTrials = 100;
     final expectedMaxDurations = [
       100, // 200, 400, 800, 1600, 3200, 6400, 10000, 10000, 10000, 10000,
     ].map((ms) => Duration(milliseconds: ms)).toList();
@@ -124,13 +92,9 @@ void main() {
     for (int i = 0; i < numTrials; i++) {
       final resultsForThisTrial = <Duration>[];
       fakeAsyncBetter((binding) async {
-        print('start $i');
         final backoffMachine = BackoffMachine();
-        print('have backoffMachine');
         for (int j = 0; j < expectedMaxDurations.length; j++) {
-          print('  step $j');
           final duration = await measureWait(backoffMachine.wait());
-          print('  -> $duration');
           resultsForThisTrial.add(duration);
         }
       });
