4d30afc2ec8ac6e30708bddaac40c16af4f76916
Greg Price <greg@zulip.com>
2024-02-09 16:21:31 -0800

    wip scratch on FakeAsync

--- test/backoff_test.dart
+++ test/backoff_test.dart
@@ -1,19 +1,39 @@
 import 'package:checks/checks.dart';
+import 'package:fake_async/fake_async.dart';
 import 'package:test/scaffolding.dart';
 import 'package:zulip/backoff.dart';
 
 Future<Duration> measureWait(Future<void> future) async {
   final start = DateTime.now();
+  print('    measure: started $start');
   await future;
+  print('    measure: done');
   return DateTime.now().difference(start);
 }
 
 void main() {
+  test('FakeAsync scratch', () async {
+    await fakeAsync((binding) async {
+      print('${DateTime.now()} ${} hi');
+
+      final delay = Future.delayed(Duration(milliseconds: 100));
+      print('${DateTime.now()} future made');
+      print(binding.pendingTimersDebugString);
+
+      binding.flushTimers();
+      print('${DateTime.now()} flushed');
+      print(binding.pendingTimersDebugString);
+
+      await delay;
+      print('${DateTime.now()} awaited');
+    });
+  });
+
   test('BackoffMachine timeouts are random from zero to 100ms, 200ms, 400ms, ...', () async {
     // This is a randomized test.  [numTrials] is chosen so that the failure
     // probability < 1e-9.  There are 2 * 11 assertions, and each one has a
     // failure probability < 1e-12; see below.
-    const numTrials = 100;
+    const numTrials = 3; //100;
     final expectedMaxDurations = [
       100, // 200, 400, 800, 1600, 3200, 6400, 10000, 10000, 10000, 10000,
     ].map((ms) => Duration(milliseconds: ms)).toList();
@@ -21,11 +41,17 @@ void main() {
     final trialResults = <List<Duration>>[];
     for (int i = 0; i < numTrials; i++) {
       final resultsForThisTrial = <Duration>[];
-      final backoffMachine = BackoffMachine();
-      for (int j = 0; j < expectedMaxDurations.length; j++) {
-        final duration = await measureWait(backoffMachine.wait());
-        resultsForThisTrial.add(duration);
-      }
+      await fakeAsync((binding) async {
+        print('start $i');
+        final backoffMachine = BackoffMachine();
+        print('have backoffMachine');
+        for (int j = 0; j < expectedMaxDurations.length; j++) {
+          print('  step $j');
+          final duration = await measureWait(backoffMachine.wait());
+          print('  -> $duration');
+          resultsForThisTrial.add(duration);
+        }
+      });
       trialResults.add(resultsForThisTrial);
     }
     print(trialResults.map((r) => r[0].inMilliseconds).toList()..sort());
