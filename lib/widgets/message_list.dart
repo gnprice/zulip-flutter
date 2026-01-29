3812c6b6f1e3aaa8472f87863f626298cade1c6e
Greg Price <greg@zulip.com>
2026-01-28 19:00:08 -0800

    api [nfc]: Make a helper dateTimeFromTimestamp

--- lib/widgets/message_list.dart
+++ lib/widgets/message_list.dart
@@ -2233,8 +2233,7 @@ enum MessageTimestampStyle {
     required ZulipLocalizations zulipLocalizations,
     required TwentyFourHourTimeMode twentyFourHourTimeMode,
   }) {
-    final asDateTime =
-      DateTime.fromMillisecondsSinceEpoch(1000 * messageTimestamp);
+    final asDateTime = dateTimeFromTimestamp(messageTimestamp);
 
     switch (this) {
       case none:     return null;
