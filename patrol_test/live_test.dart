f261eb35996e8d98c59f3c55a8138a4940a289c8
Greg Price <greg@zulip.com>
2026-02-18 16:29:26 -0800

    notif test: With Patrol, generate (from other user), view, open
    Now we have an end-to-end test that our notifications work!
    
    This seems to work reliably for me in an Android emulator.
    It might be flaky if the notification is slow to arrive;
    if we run into that, we can adjust this to handle it.
    (We're not yet running these in CI or otherwise routinely,
    so some flakiness isn't a big deal at this stage.)

--- patrol_test/live_test.dart
+++ patrol_test/live_test.dart
@@ -1,13 +1,18 @@
 // ignore_for_file: invalid_use_of_visible_for_testing_member
 
+import 'dart:math';
+
 import 'package:checks/checks.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_checks/flutter_checks.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
+import 'package:zulip/api/core.dart';
+import 'package:zulip/api/route/messages.dart';
 import 'package:zulip/main.dart';
 import 'package:zulip/model/binding.dart';
 import 'package:zulip/widgets/app.dart';
+import 'package:zulip/widgets/store.dart';
 
 void main() {
   mainInit();
@@ -16,6 +21,15 @@ void main() {
   // and the same real device platform, by the nature of live tests.
   // So we might as well let them use the same real database and global store.
   ZulipBinding.instance.debugRelaxGetGlobalStoreUniquely = true;
+
+  ApiConnection makeOtherConnection() {
+    return ApiConnection.live(
+      realmUrl: Uri.parse(const String.fromEnvironment('REALM_URL')),
+      email: const String.fromEnvironment('OTHER_EMAIL'),
+      apiKey: const String.fromEnvironment('OTHER_API_KEY'),
+      zulipFeatureLevel: 468, // TODO get the real value
+    );
+  }
 
   patrolTest('login', ($) async {
     addTearDown(ZulipApp.debugReset);
@@ -42,5 +56,36 @@ void main() {
     await $(findUsernameInput).enterText(const String.fromEnvironment('EMAIL'));
     await $(findPasswordInput).enterText(const String.fromEnvironment('PASSWORD'));
     await $.tap($(find.widgetWithText(ElevatedButton, 'Log in')));
+  });
+
+  patrolTest('notification', ($) async {
+    addTearDown(ZulipApp.debugReset);
+    await $.pumpWidgetAndSettle(ZulipApp());
+    // Already logged in by the test above.
+
+    final navigator = await ZulipApp.navigator;
+    final context = navigator.context;
+    if (!context.mounted) throw Error();
+    final globalStore = GlobalStoreWidget.of(context);
+    final account = globalStore.accounts.single;
+    final store = globalStore.getAccount(account.id)!;
+
+    await Future<void>.delayed(Duration(milliseconds: 500));
+
+    final token = Random().nextInt(1 << 32).toRadixString(16).padLeft(8, '0');
+    final content = 'hello $token';
+    final otherConnection = makeOtherConnection();
+    await sendMessage(otherConnection,
+      destination: DmDestination(userIds: [store.userId]),
+      content: content);
+
+    await $.platform.mobile.openNotifications();
+    // TODO poll? in a loop, with a timeout
+    final notifs = await $.platform.mobile.getNotifications();
+    check(notifs).isNotEmpty();
+
+    await $.platform.mobile.tapOnNotificationByIndex(0);
+    await $.waitUntilVisible($(RegExp(r'^DMs with')));
+    check($(content)).findsOne();
   });
 }
