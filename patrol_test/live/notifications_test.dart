332aacd8e1d8cbfaee55f82e5f1622ecd5a76685
Greg Price <greg@zulip.com>
2026-02-23 17:43:06 -0800

    patrol [nfc]: Make addAccount helper for live tests

--- patrol_test/live/notifications_test.dart
+++ patrol_test/live/notifications_test.dart
@@ -6,7 +6,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
 import 'package:zulip/api/route/messages.dart';
 import 'package:zulip/main.dart';
-import 'package:zulip/model/binding.dart';
 import 'package:zulip/widgets/app.dart';
 
 import 'binding.dart';
@@ -17,11 +16,7 @@ void main() {
 
   patrolTest('notification', ($) async {
     await patrolLiveBinding.reset();
-
-    final globalStore = await ZulipBinding.instance.getGlobalStore();
-    await globalStore.insertAccount(LiveCredentials.account().toCompanion(false));
-    final account = globalStore.accounts.single;
-    await globalStore.setLastVisitedAccount(account.id);
+    final account = await patrolLiveBinding.addAccount(LiveCredentials.account());
 
     await $.pumpWidget(ZulipApp());
     await $.waitUntilVisible($('Inbox'));
@@ -30,18 +25,13 @@ void main() {
       await $.platform.mobile.grantPermissionWhenInUse();
     }
 
-    final navigator = await ZulipApp.navigator;
-    final context = navigator.context;
-    if (!context.mounted) throw Error();
-    final store = globalStore.getAccount(account.id)!;
-
     await Future<void>.delayed(Duration(milliseconds: 500));
 
     final token = Random().nextInt(1 << 32).toRadixString(16).padLeft(8, '0');
     final content = 'hello $token';
     final otherConnection = LiveCredentials.makeOtherConnection();
     await sendMessage(otherConnection,
-      destination: DmDestination(userIds: [store.userId]),
+      destination: DmDestination(userIds: [account.userId]),
       content: content);
 
     await $.platform.mobile.openNotifications();
